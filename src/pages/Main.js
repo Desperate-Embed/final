96420cc980d82b35797d8f4357f3618a0466efe9
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-12-02 22:58:54 +0900

    copy

---
+++ src/pages/Main.js
@@ -0,0 +1,55 @@
+import { Header } from "../components/Header";
+import { Link } from "react-router-dom";
+import "../css/Main.css";
+import { motion } from "framer-motion";
+
+export function Main() {
+  return (
+    <>
+      <div className="Mainbody">
+        <Header />
+        <div className="Boxcenter">
+          <span className="Box관리자용">관리자용</span>
+          <div className="Boxspace">
+            <Link to="/Manage" style={{ textDecoration: "none" }}>
+              <motion.div
+                className="Box"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+                <div className="세로">
+                  <img className="boximg" src="/image/하수구관리.png"></img>
+                  <span className="boxtext">하수구 관리</span>
+                </div>
+              </motion.div>
+            </Link>
+            <Link to="/Location" style={{ textDecoration: "none" }}>
+              <motion.div
+                className="Box"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+                <div className="세로">
+                  <img className="boximg" src="/image/위치확인.png"></img>
+                  <span className="boxtext">위치 확인</span>
+                </div>
+              </motion.div>
+            </Link>
+            <Link to="/Check" style={{ textDecoration: "none" }}>
+              <motion.div
+                className="Box"
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+              >
+                <div className="세로">
+                  <img className="boximg" src="/image/쓰레기확인.png"></img>
+                  <span className="boxtext">쓰레기 확인</span>
+                </div>
+              </motion.div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
